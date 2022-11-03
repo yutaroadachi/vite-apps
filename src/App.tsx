69f28a51096c75094e2d292d1588f6992d8c44fe
yutaroadachi <ytr_adc72@yahoo.co.jp>
2022-11-03 18:09:14 +0900

    feat: Tailwind CSSを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import routes from "./routes/routes"
+import "./styles/global.css"
 
 const router = createBrowserRouter(routes)
 
